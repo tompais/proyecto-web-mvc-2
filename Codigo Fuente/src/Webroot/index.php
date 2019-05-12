982ccc318bbef469c739cea60e4ea66f444c5c98
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-05-12 14:47:46 -0300

    Muevo throwError404 para ser accesible globalmente

--- Codigo Fuente/src/Webroot/index.php
+++ Codigo Fuente/src/Webroot/index.php
@@ -9,11 +9,19 @@ require_once ROOT . 'router.php';
 require_once ROOT . 'request.php';
 require_once ROOT . 'dispatcher.php';
 
-function getBaseAddress() {
+function getBaseAddress()
+{
     $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
-    $filenamepattern = '/'.basename(__FILE__).'/';
-    $url = $protocol.'://'.$_SERVER['HTTP_HOST'].preg_replace($filenamepattern,"",$_SERVER['REQUEST_URI']);
+    $filenamepattern = '/' . basename(__FILE__) . '/';
+    $url = $protocol . '://' . $_SERVER['HTTP_HOST'] . preg_replace($filenamepattern, "", $_SERVER['REQUEST_URI']);
     return substr_replace($url, "", strripos($url, "/src/") + 5);
+}
+
+function throwError404()
+{
+    http_response_code(404);
+    include ROOT . "Views/NoCompletado/noCompletado.php";
+    die();
 }
 
 session_start();
