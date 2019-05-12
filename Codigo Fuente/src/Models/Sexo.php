2ee06e55c4b7eae57cd3c52d3ecd54497a9ae240
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-05-12 13:53:11 -0300

    Agrego select de sexos

---
+++ Codigo Fuente/src/Models/Sexo.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Globons
+ * Date: 12/5/2019
+ * Time: 13:13
+ */
+
+class Sexo extends Model
+{
+    private $id;
+    private $nombre;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+}
