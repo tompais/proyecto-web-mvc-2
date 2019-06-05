81b4e0d3ebc9166aede4593a9cb0da2d4d7ab7d3
Alejovoley14 <pupe893@gmail.com>
2019-06-05 19:44:14 -0300

    mejoro publicacion

--- Codigo Fuente/src/Controllers/PublicacionController.php
+++ Codigo Fuente/src/Controllers/PublicacionController.php
@@ -16,16 +16,38 @@ class PublicacionController extends Controller
         $categoria = new Categoria();
         $usuario = new Usuario();
         $direccion = new Direccion();
+        $sesion = unserialize($_SESSION["session"]);
+
 
         $producto->traerProducto($publicacion["producto"]);
         $categoria->traerCategoria($producto->getCategoriaId());
         $usuario->traerUsuario($producto->getUsuarioId());
-        //$direccion->traerDireccion($usuario->getDireccionId());
+
+        $productos = "";
+
+        if ($productos = $producto->traerListaProductos($sesion->getId())) {
+            $imagen = new Imagen();
+
+            $imagenes = [];
+
+            foreach ($productos as $producto) {
+                $imgProduc = $imagen->traerListaImagenes($producto->getId());
+
+                foreach ($imgProduc as $imgP)
+                    array_push($imagenes, $imgP);
+            }
+
+            $d["productos"] = $productos;
+            $d["imagenes"] = $imagenes;
+        }
+
+        $d["productos"] = $productos;
+        $d["imagenes"] = $imagenes;
 
         $d["producto"] = $producto;
         $d["categoria"] = $categoria;
         $d["usuario"] = $usuario;
-        //$d["direccion"] = $direccion;
+
 
         $this->set($d);
         $this->render(Constantes::PUBLICACIONVIEW);
