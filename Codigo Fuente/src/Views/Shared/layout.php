bc166ec938dc44d7666d5305dde6c58dd2d01bcc
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-06-09 13:23:36 -0300

    Resuelvo problemas de fuentes

--- Codigo Fuente/src/Views/Shared/layout.php
+++ Codigo Fuente/src/Views/Shared/layout.php
@@ -18,12 +18,13 @@
     <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/lib/alertifyjs/css/themes/default.min.css"; ?>">
     <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/lib/alertifyjs/css/themes/semantic.min.css"; ?>">
     <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/lib/daterangepicker/daterangepicker.css"; ?>">
-    <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/home/Inicio.css"; ?>">
-    <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/home/responsive.css"; ?>">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/error/errorValidaciones.css"; ?>">
     <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/lib/Semantic-UI/dist/semantic.min.css"; ?>">
     <link rel="shortcut icon" href="<?php echo getBaseAddress() . "Webroot/img/favicon.ico"; ?>" type="image/x-icon">
     <link rel="icon" href="<?php echo getBaseAddress() . "Webroot/img/favicon.ico"; ?>" type="image/x-icon">
+    <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/home/Inicio.css"; ?>">
+    <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/home/responsive.css"; ?>">
 </head>
 
 <body>
