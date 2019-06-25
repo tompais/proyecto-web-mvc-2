a4352cd3c61431ebaeed003f2ea06bb6b007b76e
Alejovoley14 <pupe893@gmail.com>
2019-06-25 17:39:17 -0300

    agrego productos relacionados en publicacion.php

--- Codigo Fuente/src/Views/Productos/publicacion.php
+++ Codigo Fuente/src/Views/Productos/publicacion.php
@@ -1,5 +1,7 @@
 <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/publicacion/publicacionProducto.css" ?>">
 <link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/css/publicacion/publicacionProductoResponsive.css" ?>">
+<link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/lib/OwlCarousel2-2.2.1/owl.carousel.css"; ?>">
+<link rel="stylesheet" href="<?php echo getBaseAddress() . "Webroot/lib/OwlCarousel2-2.2.1/owl.theme.default.css"; ?>">
 
 <div class="container single_product_container">
     <div class="row">
@@ -65,7 +67,8 @@
                     <span class="ti-truck"></span><span>Caracteristicas</span>
                 </div>
 
-                <div class="product_price mt-5">$ <?php echo $producto->getPrecio() ?></div>
+                <div class="product_price mt-5" style="font-size: 24px">Precio: $<?php echo $producto->getPrecio() ?></div><br>
+                <div class="product_price mt-5" style="font-size: 24px">Cantidad: <?php echo $producto->getCantidad() ?> (disponibles) </div>
 
                 <br>
                 <br>
@@ -86,7 +89,7 @@
             <div class="row">
                 <div class="col">
                     <div class="tabs_container">
-                        <ul class="tabs d-flex flex-sm-row flex-column align-items-left align-items-md-center justify-content-center">
+                        <ul class="tabs d-flex flex-sm-row flex-column align-items-md-center justify-content-center">
                             <li class="tab active" data-active-tab="tab_1"><span>Descripcion</span></li>
                             <li class="tab" data-active-tab="tab_3"><span>Comentarios</span></li>
                         </ul>
@@ -99,14 +102,102 @@
                     <!-- Tab Description -->
 
                     <div id="tab_1" class="tab_container active">
-                        <div class="row">
-                            <div class="col-lg-5 desc_col">
-                                <div class="tab_title">
-                                    <h4>Descrpcion</h4>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col text-center">
+                                    <div class="section_title">
+                                        <h2>Productos Relacionados</h2>
+                                    </div>
                                 </div>
-                                <div class="tab_text_block">
-                                    <h2>Pocket cotton sweatshirt</h2>
-                                    <p>Nam tempus turpis at metus scelerisque placerat nulla deumantos solicitud felis. Pellentesque diam dolor, elementum etos lobortis des mollis ut...</p>
+                            </div>
+                            <div class="row mt-5">
+                                <div class="col">
+                                    <div class="owl-carousel owl-theme product_slider">
+                                        <!-- Producto 1 -->
+                                        <div class="owl-item product_slider_item">
+                                            <div class="product-item">
+                                                <div class="product discount">
+                                                    <div class="product_image">
+                                                        <img src="<?php echo getBaseAddress() . "Webroot/img/home/product_1.png" ?>" alt="">
+                                                    </div>
+                                                    <div class="product_info">
+                                                        <h6 class="product_name"><a href="#">Buzo Bordo con capucha</a></h6>
+                                                        <div class="product_price">$520.00</div>
+                                                        <button class="btn btn-primary btn-block btn-sm" style="background: #0099df"><i class="fab fa-opencart mr-1"></i>Agregar al Carrito</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Producto 2 -->
+
+                                        <div class="owl-item product_slider_item">
+                                            <div class="product-item women">
+                                                <div class="product">
+                                                    <div class="product_image">
+                                                        <img src="<?php echo getBaseAddress() . "Webroot/img/home/product_2.png" ?>" alt="">
+                                                    </div>
+                                                    <div class="product_info">
+                                                        <h6 class="product_name"><a href="#">Samsung S8 Plus</a></h6>
+                                                        <div class="product_price">$50000.00</div>
+                                                        <button class="btn btn-primary btn-block btn-sm" style="background: #0099df"><i class="fab fa-opencart mr-1"></i>Agregar al Carrito</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Producto 3 -->
+
+                                        <div class="owl-item product_slider_item">
+                                            <div class="product-item women">
+                                                <div class="product">
+                                                    <div class="product_image">
+                                                        <img src="<?php echo getBaseAddress() . "Webroot/img/home/product_3.png" ?>" alt="">
+                                                    </div>
+                                                    <div class="product_info">
+                                                        <h6 class="product_name"><a href="#">Nike Airmax 97</a></h6>
+                                                        <div class="product_price">$2500.00</div>
+                                                        <button class="btn btn-primary btn-block btn-sm" style="background: #0099df"><i class="fab fa-opencart mr-1"></i>Agregar al Carrito</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Producto 4 -->
+
+                                        <div class="owl-item product_slider_item">
+                                            <div class="product-item accessories">
+                                                <div class="product">
+                                                    <div class="product_image">
+                                                        <img src="<?php echo getBaseAddress() . "Webroot/img/home/product_4.png" ?>" alt="">
+                                                    </div>
+                                                    <div class="product_info">
+                                                        <h6 class="product_name"><a href="#">Moto G5 Plus</a></h6>
+                                                        <div class="product_price">$6000.00</div>
+                                                        <button class="btn btn-primary btn-block btn-sm" style="background: #0099df"><i class="fab fa-opencart mr-1"></i>Agregar al Carrito</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Producto 5 -->
+
+                                        <div class="owl-item product_slider_item">
+                                            <div class="product-item women men">
+                                                <div class="product">
+                                                    <div class="product_image">
+                                                        <img src="<?php echo getBaseAddress() . "Webroot/img/home/product_5.png" ?>"s alt="">
+                                                    </div>
+                                                    <div class="product_info">
+                                                        <h6 class="product_name"><a href="#">Puma Active 600</a></h6>
+                                                        <div class="product_price">$1000.00</div>
+                                                        <button class="btn btn-primary btn-block btn-sm" style="background: #0099df"><i class="fab fa-opencart mr-1"></i>Agregar al Carrito</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -281,5 +372,5 @@
 </div>
 
 
-
+<script src="<?php echo getBaseAddress() . "Webroot/lib/OwlCarousel2-2.2.1/owl.carousel.js"; ?>"></script>
 <script src="<?php echo getBaseAddress() . "Webroot/js/publicacion/publicacionProducto.js" ?>"></script>
