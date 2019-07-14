04c8bba985c0e3f0c2edb22620e93dda5b64628b
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-07-14 10:56:12 -0300

    Arreglo problema de dibujado en facturaciones mensuales

--- Codigo Fuente/src/Webroot/js/dashboard/facturacionesMensuales.js
+++ Codigo Fuente/src/Webroot/js/dashboard/facturacionesMensuales.js
@@ -11,6 +11,7 @@ var tableId = null;
 inicializarDateRangePicker();
 
 function cargarTablas(registrosComprasPorMes) {
+   divCardsContainer.empty();
    if(registrosComprasPorMes === null || registrosComprasPorMes.length === 0) {
       advertencia.removeClass('d-none');
    } else {
@@ -198,7 +199,7 @@ function facturacionExitoso (dummy) {
    tableId = null;
    btnFacturar = null;
    if(divCardsContainer.length === 1) {
-      divCardsContainer.removeClass('d-flex').addClass('d-none');
+      divCardsContainer.removeClass('d-flex').addClass('d-none').empty();
       advertencia.removeClass('d-none');
    }
    alertify.alert('Facturación exitosa', 'Se ha realizado la facturación correspondiente');
